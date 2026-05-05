9a501ca0c889a0f6f4f67092f574b9b75f555bf1
SeenThai <seenza123456@gmail.com>
2026-05-05 16:42:59 +0700

    add all files

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const sidebar = document.getElementById("sidebar");
 menuBtn.addEventListener("click", () => {
     sidebar.classList.toggle("active");
 });
+
+document.addEventListener('contextmenu', e => e.preventDefault());
+document.addEventListener('dragstart', e => e.preventDefault());
